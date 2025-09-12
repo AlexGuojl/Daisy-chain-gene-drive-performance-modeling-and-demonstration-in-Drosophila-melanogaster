d5fe22fe746fedcb0a5dda130f84b15216899c91
AlexGuojl <109111350+AlexGuojl@users.noreply.github.com>
2025-09-12 11:50:24 +0800

    Add files via upload

---
+++ 1Dspace_vary_conversion/wave_data_vary_conversion2.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH -J gjl_1d_dc
+#SBATCH -p cn-long
+#SBATCH -N 1
+#SBATCH -o gjl_con_sup_%j.out
+#SBATCH -e gjl_con_sup_%j.err
+#SBATCH --no-requeue
+#SBATCH -A jchamper_g1
+#SBATCH --qos=jchampercnl
+#SBATCH -c 1
+pkurun  sleep 1
+
+mkdir -p conversion_vs_dropsize
+
+drop_size=(0 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0);
+
+drive_conversion=(0 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0);
+
+
+for ((i=1;i<11;i++)) #repeat times
+do
+    for ((j=1;j<12;j++)) #j traverse drop_size 
+    do
+        for ((m=2;m<3;m++))  #  traverse drive_conversion
+        do
+            k=$((($i+10)*10000+($j+10)*100+$m+10)) # cautious about file order 1 > 02 110000+11*100+11
+            if (($k==111111));then
+            python3 daisy_1d_driver_con.py -drop_size ${drop_size[$m]} -conversion_rate ${drive_conversion[$j]} -header > conversion_vs_dropsize/$k.part
+            else
+            python3 daisy_1d_driver_con.py -drop_size ${drop_size[$m]} -conversion_rate ${drive_conversion[$j]} > conversion_vs_dropsize/$k.part
+            fi
+        done
+    done
+done
+
+
+wait
+cd conversion_vs_dropsize
+
+
+cd ..
+
